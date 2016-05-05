c39c36c4d94a563994756626e3bb7cd9ccb8d26c
Alice Boxhall <aboxhall@chromium.org>
2016-05-05 11:03:51 -0700

    More polish for menubutton

--- lesson5-semantics-aria/13-listbox/menubutton.js
+++ lesson5-semantics-aria/13-listbox/menubutton.js
@@ -10,94 +10,174 @@
   var VK_RIGHT      = 39;
   var VK_DOWN       = 40;
 
-  function toggleMenu() {
-    if (menu.hasAttribute('hidden')) {
-      menu.removeAttribute('hidden');
-      menu.focus();
-    } else {
-      closeMenu();
-    }
+  function PopUpButton(buttonEl, menuEl) {
+    this.buttonEl = buttonEl;
+    this.menu = new Menu(menuEl, this);
+
+    this.buttonEl.addEventListener('click', this.toggleMenu.bind(this));
+    this.buttonEl.addEventListener('keydown', this.handleKeyDown.bind(this));
   }
 
-  function closeMenu() {
-    menu.setAttribute('hidden', '');
-  }
+  PopUpButton.prototype = {
+    focus: function() {
+      this.buttonEl.focus();
+    },
 
-  function handleKeyDownOnButton(e) {
-    switch (e.keyCode) {
+    toggleMenu: function() {
+      this.menu.toggle();
+    },
+
+    set value(val) {
+      this.buttonEl.textContent = val;
+    },
+
+    handleKeyDown: function(e) {
+      switch (e.keyCode) {
       case VK_DOWN:
       case VK_ENTER:
       case VK_SPACE:
-        if (!menu.hasAttribute('hidden'))
+        if (!this.menu.hidden)
           break;
-        changeActiveListitem(0);
-        toggleMenu();
+        this.menu.changeActiveListitem(0);
+        this.menu.show();
+        e.stopPropagation();
+        e.preventDefault();
         break;
+      }
+      return;
     }
-    return;
+  };
+
+  function Menu(el, button) {
+    this.el = el;
+    this.button = button;
+    this.items = Array.prototype.slice.call(el.querySelectorAll('[role=menuitem]'));
+    for (var item of this.items) {
+      item.addEventListener('mouseover', this.handleHoverOnItem.bind(this));
+      item.addEventListener('click', this.handleClickOnItem.bind(this));
+    }
+
+    el.addEventListener('keydown', this.handleKeyDown.bind(this));
+    el.addEventListener('blur', this.hide.bind(this));
   }
 
-  var popupbutton = document.querySelector('button[aria-haspopup]');
-  popupbutton.addEventListener('click', toggleMenu);
-  popupbutton.addEventListener('keydown', handleKeyDownOnButton);
 
-  var menu = document.querySelector('[role=menu]');
-  var listitems = Array.prototype.slice.call(menu.querySelectorAll('[role=menuitem]'));
+  Menu.prototype = {
+    toggle: function() {
+      if (this.hidden) {
+        this.show();
+      } else {
+        this.hide();
+      }
+    },
 
+    get hidden() {
+      return this.el.hasAttribute('hidden');
+    },
 
-  function handleKeyDown(e) {
-    var active = menu.querySelector('[role=menuitem][active]');
-    if (!active)
-      activeIdx = -1;
-    else
-      var activeIdx = listitems.indexOf(active);
+    get activeItem() {
+      return this.el.querySelector('[role=menuitem][active]');
+    },
 
-    var newIdx = activeIdx;
-    switch(e.keyCode) {
+    show: function() {
+      if (!this.hidden)
+        return;
+
+      this.el.removeAttribute('hidden');
+      this.el.focus();
+    },
+
+    hide: function() {
+      if (this.hidden)
+        return;
+
+      this.activeItem.removeAttribute('active');
+      this.el.setAttribute('hidden', '');
+      this.button.focus();
+    },
+
+    selectItem: function(item) {
+      var selected = this.el.querySelector('[aria-selected]');
+      if (selected && selected != item)
+        selected.removeAttribute('aria-selected');
+      item.setAttribute('aria-selected', true);
+
+      this.button.value = item.textContent;
+      this.hide();
+    },
+
+    handleKeyDown: function (e) {
+      var active = this.activeItem;
+      if (!active)
+        activeIdx = -1;
+      else
+        var activeIdx = this.items.indexOf(active);
+
+      var newIdx = activeIdx;
+      switch(e.keyCode) {
       case VK_UP:
       case VK_LEFT:
         if (!active)
-          newIdx = listitems.length;
+          newIdx = this.items.length;
 
         newIdx--;
         if (newIdx < 0)
-          newIdx += listitems.length;
+          newIdx += this.items.length;
+        e.stopPropagation();
+        e.preventDefault();
         break;
 
       case VK_DOWN:
       case VK_RIGHT:
-        newIdx = (newIdx + 1) % listitems.length;
+        newIdx = (newIdx + 1) % this.items.length;
+        e.stopPropagation();
+        e.preventDefault();
         break;
 
       case VK_SPACE:
       case VK_ENTER:
-        var selected = menu.querySelector('[aria-selected]');
-        if (selected && selected != active)
-          selected.removeAttribute('aria-selected');
-        active.setAttribute('aria-selected', true);
-        popupbutton.textContent = active.textContent;
-        closeMenu();
-        popupbutton.focus();
+        this.selectItem(active);
+        e.stopPropagation();
+        e.preventDefault();
         break;
 
       case VK_ESC:
-        closeMenu();
+        e.stopPropagation();
+        e.preventDefault();
+        this.hide();
+      }
+
+      if (newIdx == activeIdx)
+        return;
+      this.changeActiveListitem(newIdx);
+    },
+
+    handleHoverOnItem: function(e) {
+      var newIdx = this.items.indexOf(e.target);
+      if (newIdx < 0)
+        return;
+      this.changeActiveListitem(newIdx);
+    },
+
+    handleClickOnItem: function(e) {
+      var item = e.target;
+      if (this.items.indexOf(item) < 0)
+        return;
+      this.selectItem(item);
+    },
+
+    changeActiveListitem: function(newIdx) {
+      var active = this.activeItem;
+      var newActive = this.items[newIdx];
+      if (active)
+        active.removeAttribute('active');
+      newActive.setAttribute('active', '');
+      this.el.setAttribute('aria-activedescendant', newActive.id);
     }
+  };
 
-    if (newIdx == activeIdx)
-      return;
-    changeActiveListitem(newIdx);
-  }
+  var popupbutton = document.querySelector('button[aria-haspopup]');
+  var menu = document.querySelector('[role=menu]');
 
-  function changeActiveListitem(newIdx) {
-    var active = menu.querySelector('[role=menuitem][active]');
-    var newActive = listitems[newIdx];
-    if (active)
-      active.removeAttribute('active');
-    newActive.setAttribute('active', '');
-    menu.setAttribute('aria-activedescendant', newActive.id);
-  }
-
-  menu.addEventListener('keydown', handleKeyDown);
-  menu.addEventListener('blur', closeMenu);
+  new PopUpButton(popupbutton, menu);
 })()
