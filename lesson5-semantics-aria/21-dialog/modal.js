632fd5f1650e6ea93eb2446fa3fb6a3ce7583de8
Alice Boxhall <aboxhall@chromium.org>
2016-05-08 18:37:08 -0700

    Fix modal example in lesson 5

--- lesson5-semantics-aria/21-dialog/modal.js
+++ lesson5-semantics-aria/21-dialog/modal.js
@@ -8,18 +8,18 @@ var modalOverlay = document.querySelector('.modal-overlay');
 var modalToggle = document.querySelector('.modal-toggle');
 modalToggle.addEventListener('click', openModal);
 
-function openModal() {
+function openModal(e) {
   // Save current focus
-  focusedElementBeforeModal = document.activeElement;
+  focusedElementBeforeModal = e.target;
 
   // Listen for and trap the keyboard
   modal.addEventListener('keydown', trapTabKey);
 
   // Listen for indicators to close the modal
   modalOverlay.addEventListener('click', closeModal);
-  // Sign-Up button
-  var signUpBtn = modal.querySelector('#signup');
-  signUpBtn.addEventListener('click', closeModal);
+  // Log In button
+  var closeButton = modal.querySelector('#close');
+  closeButton.addEventListener('click', closeModal);
 
   // Find all focusable children
   var focusableElementsString = 'a[href], area[href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), iframe, object, embed, [tabindex="0"], [contenteditable]';
